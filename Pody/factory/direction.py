62673c7798be0f92b43a93589509842e3feb8eaa
TheRake66 <65202670+TheRake66@users.noreply.github.com>
2022-09-19 22:46:13 +0200

    Update direction.py

--- Pody/factory/direction.py
+++ Pody/factory/direction.py
@@ -5,5 +5,5 @@ class Direction:
     """Enumération des types de direction de tri.
     """
     
-    ASC = 'ASC'  # type: int # ASC
+    ASC = 'ASC'    # type: int # ASC
     DESC = 'DESC'  # type: int # DESC
